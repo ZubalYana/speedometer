17521ef39472fd30358d868497e4ca04e766bc8d
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-10-08 05:40:36 +0300

    display the statistics chart with some of the speed history data, prevent the app from throwing mistakes ( add a placeholdered note if no speed history data available )

--- components/StatisticScreen.js
+++ components/StatisticScreen.js
@@ -1,17 +1,51 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { useEffect } from 'react';
+import { LineChart } from 'react-native-chart-kit';
+import { MaterialIcons } from '@expo/vector-icons';
+
 export default function StatisticScreen({ speedHistory }) {
     useEffect(() => {
         console.log('App speedHistory:', speedHistory);
     }, [speedHistory]);
+    const screenWidth = Dimensions.get('window').width;
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>📊 Statistic Screen</Text>
+            <View style={{ flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
+                <MaterialIcons
+                    name='bar-chart'
+                    size={40}
+                    color='#f1f1f1ff'
+                />
+                <Text style={styles.text}>
+                    Live statistics & history
+                </Text>
+            </View>
+            {speedHistory.length ?
+                <LineChart
+                    data={{
+                        labels: Array(speedHistory.length).fill(''),
+                        datasets: [{ data: speedHistory }],
+                    }}
+                    width={screenWidth - 40}
+                    height={220}
+                    yAxisSuffix=" m/s"
+                    chartConfig={{
+                        decimalPlaces: 2,
+                        color: (opacity = 1) => `rgba(255, 14, 14, ${opacity})`,
+                        labelColor: () => '#fff',
+                    }}
+                    style={styles.chart}
+                />
+                :
+                <Text style={{ color: '#f1f1f1ff', paddingHorizontal: 40, textAlign: 'center' }}>No data available for the chart so far. Start measuring your speed and take a look!</Text>
+            }
+
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0f0f0fff', width: '100%' },
-    text: { color: '#f1f1f1ff', fontSize: 22 },
+    text: { color: '#f1f1f1ff', fontSize: 22, fontWeight: 700, marginTop: 5 },
+    chart: { marginTop: 50 }
 });
